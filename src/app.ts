2a0ee0a2ae54b96236dbc22ad8c8ce6c724d3952
Sarthak Pandey <sarthak@email.com>
2026-08-16 08:29:00 +0530

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+
+import express from 'express';
+
+const app = express();
+
+
+
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+  });
+});
+
+
+export default app;
